a445072982ae67e7e18fe28cbbc2221f73b6ced0
adivrskic <adivrskic123@gmail.com>
2025-01-30 13:42:29 -0500

    fix build failure

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 export default {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
+    domains: ["cdn.sanity.io"], // Allow images from Sanity's CDN
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@sanity/config": path.resolve(__dirname, "sanity/sanity.config.js"),
+    };
+    return config;
   },
 };
