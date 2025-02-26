3794cd8fb675b6fa83b3856da60eecfcdd1b074c
Adi Vrskic <adi_vrskic1@homedepot.com>
2025-02-26 17:37:54 -0500

    remove hidden

--- sanity/schemaTypes/banner.js
+++ sanity/schemaTypes/banner.js
@@ -1,46 +1,45 @@
 export default {
-  name: 'banner', // Unique identifier for this schema
-  title: 'Banner', // Title shown in Sanity Studio
-  type: 'document', // Sanity document schema
+  name: "banner",
+  title: "Banner",
+  type: "document",
   fields: [
     {
-      name: 'image',
-      title: 'Banner Image',
-      type: 'image', // This is to upload an image for the banner
+      name: "image",
+      title: "Banner Image",
+      type: "image",
       options: {
-        hotspot: true, // Enable the hotspot feature for cropping
+        hotspot: true,
       },
     },
     {
-      name: 'title',
-      title: 'Banner Title',
-      type: 'string', // This will store the title of the banner
+      name: "title",
+      title: "Banner Title",
+      type: "string",
     },
     {
-      name: 'text',
-      title: 'Banner Text',
-      type: 'text', // This will store the description text for the banner
+      name: "text",
+      title: "Banner Text",
+      type: "text",
     },
     {
-      name: 'button1Link',
-      title: 'Button 1 Link',
-      type: 'string', // Link for the first button
+      name: "button1Link",
+      title: "Button 1 Link",
+      type: "string",
     },
     {
-      name: 'button1Text',
-      title: 'Button 1 Text',
-      type: 'string', // Text for the first button
+      name: "button1Text",
+      title: "Button 1 Text",
+      type: "string",
     },
     {
-      name: 'button2Link',
-      title: 'Button 2 Link',
-      type: 'string', // Link for the second button
+      name: "button2Link",
+      title: "Button 2 Link",
+      type: "string",
     },
     {
-      name: 'button2Text',
-      title: 'Button 2 Text',
-      type: 'string', // Text for the second button
+      name: "button2Text",
+      title: "Button 2 Text",
+      type: "string",
     },
   ],
-  hidden: true,
 };
